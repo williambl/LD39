01538a5104ce4455a7e21fb5f3bf2a755d181ae5
williambl <william@williambl.com>
2017-07-29 21:29:54 +0100

    Score now starts at 100 and ticks down.

--- LD39/Assets/Scripts/ScoreManager.cs
+++ LD39/Assets/Scripts/ScoreManager.cs
@@ -5,17 +5,22 @@ using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour {
 
-	static int score = 0;
+	static int score = 100;
 
 	public static Text scoreText;
 
 	void Start () {
 		scoreText = GetComponent<Text>();
+		InvokeRepeating("TickingScore", 1, 1);
 	}
 
 	public static void AddScore (int input) {
 		score += input;
 		scoreText.text = score.ToString();
+	}
+
+	void TickingScore () {
+		AddScore(-1 * (int)SpeedManager.speedMultiplier);
 	}
 
 }
---
+++ LD39/Assets/Scripts/SpeedManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedManager : MonoBehaviour {
+
+	public static float speedMultiplier = 1;
+
+	void Start () {
+		InvokeRepeating("TickingSpeed", 0.5f, 0.5f);
+	}
+
+	public static void AddSpeed (float input)
+	{
+		speedMultiplier += input;
+	}
+
+	void TickingSpeed ()
+	{
+		AddSpeed(0.1f);
+	}
+
+}
