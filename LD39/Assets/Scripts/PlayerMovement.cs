e10b47a5717db464fbb57d8b5503010505942a84
williambl <william@williambl.com>
2017-07-29 22:05:27 +0100

    You can no longer escape the level

--- LD39/Assets/Scripts/PlayerMovement.cs
+++ LD39/Assets/Scripts/PlayerMovement.cs
@@ -13,7 +13,8 @@ public class PlayerMovement : MonoBehaviour
 	{
 		var x = Input.GetAxis("Horizontal") * Time.deltaTime * rotateSpeed * SpeedManager.speedMultiplier;
         var z = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed * SpeedManager.speedMultiplier;
-		z = Mathf.Clamp(z, 0, Mathf.Infinity);
+        z = Mathf.Clamp(z, 0, Mathf.Infinity);
+		x = Mathf.Clamp(x, -4-transform.position.x, 4-transform.position.x);
         transform.Translate(x, 0, z);
 		level.Translate(0, 0, z);
 	}
