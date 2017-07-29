a5222cc61a91c25bb18487c97fed5ebe97c68e71
williambl <william@williambl.com>
2017-07-29 18:58:24 +0100

    Level now moves with you

--- LD39/Assets/Scripts/PlayerMovement.cs
+++ LD39/Assets/Scripts/PlayerMovement.cs
@@ -7,12 +7,14 @@ public class PlayerMovement : MonoBehaviour
 
     float rotateSpeed = 30;
     float moveSpeed = 10;
+    public Transform level;
 
     void FixedUpdate()
 	{
 		var x = Input.GetAxis("Horizontal") * Time.deltaTime * rotateSpeed;
         var z = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
 		z = Mathf.Clamp(z, 0, Mathf.Infinity);
-		transform.Translate (x, 0, z);
+        transform.Translate(x, 0, z);
+		level.Translate(0, 0, z);
 	}
 }
