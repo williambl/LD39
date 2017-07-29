614fd62a84a4d932ca61e79e895f786626a4c7c0
williambl <william@williambl.com>
2017-07-29 23:38:19 +0100

    Obstacles now spawn with regular items

--- LD39/Assets/Scripts/GenerateItems.cs
+++ LD39/Assets/Scripts/GenerateItems.cs
@@ -4,25 +4,27 @@ using UnityEngine;
 
 public class GenerateItems : MonoBehaviour {
 
-	public GameObject item;
+	public GameObject[] items;
 	public GameObject player;
 
 	void Start () {
 		for (int i = 0; i < Random.Range(20, 50); i++) {
+				GameObject toSpawn = items[Random.Range(0, items.Length)];
 				Vector3 position = player.transform.position;
 				position += new Vector3(0, 0.1f, Random.Range(5, 200));
 				position.x = Random.Range(-4, 4);
-				Instantiate(item, position, Quaternion.Euler(Vector3.zero));
+				Instantiate(toSpawn, position, Quaternion.Euler(Vector3.zero));
 		}
 	}
 
 	void Update () {
 
 		if (Random.Range(0f, 1f) > 0.91f) {
+			GameObject toSpawn = items[Random.Range(0, items.Length)];
 			Vector3 position = player.transform.position;
 			position += new Vector3(0, 0.1f, Random.Range(100, 200));
 			position.x = Random.Range(-4, 4);
-			Instantiate(item, position, Quaternion.Euler(Vector3.zero));
+			Instantiate(toSpawn, position, Quaternion.Euler(Vector3.zero));
 		}
 
 	}
