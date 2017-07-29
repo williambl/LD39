4b9a45dc31ede04767833c3530ffdd7d4fda1a67
williambl <william@williambl.com>
2017-07-30 00:16:10 +0100

    Added overpowered items, which give you 5 points instead of 1.

--- LD39/Assets/Scripts/Item.cs
+++ LD39/Assets/Scripts/Item.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class Item : MonoBehaviour {
 
 	public int score;
+	public Material overpoweredMaterial;
+
+	void Start ()
+	{
+		if (Random.Range(0f, 1f) > 0.75 && score > 0) {
+			GetComponent<Renderer>().material = overpoweredMaterial;
+			score = 5;
+        }
+	}
 
 	void OnTriggerEnter (Collider other) {
 		ScoreManager.AddScore(score);
