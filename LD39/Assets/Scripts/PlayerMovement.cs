7c7ca81618bf79919322c6f29c505e2a115bec05
williambl <william@williambl.com>
2017-07-29 15:18:29 +0100

    Added player movement

---
+++ LD39/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    float rotateSpeed = 30;
+    float moveSpeed = 10;
+
+    void FixedUpdate()
+	{
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * rotateSpeed;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+		transform.Translate (x, 0, z);
+	}
+}
