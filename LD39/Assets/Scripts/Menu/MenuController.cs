ab648068305436c56ff6826fb223fd29c12c520b
williambl <william@williambl.com>
2017-07-30 20:16:36 +0100

    Created a menu

---
+++ LD39/Assets/Scripts/Menu/MenuController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+	int status = 0;
+
+	public GameObject instructions;
+	public GameObject objects;
+
+	void Update () {
+		if (Input.GetButtonDown("Submit")) {
+			status++;
+		}
+
+		switch (status) {
+			case 0:
+			instructions.SetActive(true);
+			objects.SetActive(false);
+			break;
+			case 1:
+			instructions.SetActive(false);
+			objects.SetActive(true);
+			break;
+			case 2:
+			SceneManager.LoadScene(1);
+			break;
+			default:
+			SceneManager.LoadScene(1);
+			break;
+		}
+	}
+}
---
+++ LD39/Assets/Scripts/Menu/Rotate.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(1, 1, 0);
+	}
+}
