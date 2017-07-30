837744f236c37c2892704a9f0320bb988e6aa4c7
williambl <william@williambl.com>
2017-07-30 11:48:27 +0100

    Move GenerateItems to the player

--- LD39/Assets/Scripts/GenerateItems.cs
+++ LD39/Assets/Scripts/GenerateItems.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 public class GenerateItems : MonoBehaviour {
 
 	public GameObject[] items;
-	public GameObject player;
 
 	void Start () {
 		for (int i = 0; i < Random.Range(20, 50); i++) {
 				GameObject toSpawn = items[Random.Range(0, items.Length)];
-				Vector3 position = player.transform.position;
+				Vector3 position = transform.position;
 				position += new Vector3(0, 0.1f, Random.Range(5, 200));
 				position.x = Random.Range(-4, 4);
 				Instantiate(toSpawn, position, Quaternion.Euler(Vector3.zero));
@@ -21,7 +20,7 @@ public class GenerateItems : MonoBehaviour {
 
 		if (Random.Range(0f, 1f) > 0.8f) {
 			GameObject toSpawn = items[Random.Range(0, items.Length)];
-			Vector3 position = player.transform.position;
+			Vector3 position = transform.position;
 			position += new Vector3(0, 0.1f, Random.Range(100, 200));
 			position.x = Random.Range(-4, 4);
 			Instantiate(toSpawn, position, Quaternion.Euler(Vector3.zero));
